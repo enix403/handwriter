959e2f181a8357e0d33d67e26c4466d6c2ad8ba6
enix403 <fog.code000@gmail.com>
2024-03-18 17:54:48 +0500

    Manage list of components

--- src/features/Home.tsx
+++ src/features/Home.tsx
@@ -1,148 +1,94 @@
 import useSize from "@react-hook/size";
 import Konva from "konva";
-import { createContext, useContext, useEffect, useRef, useState } from "react";
-import { Layer, Shape, Stage } from "react-konva";
-import { isCoreLoaded, wasmCore } from "@/tunnel";
-import { useObject } from "./hooks";
-import { Vector2d } from "konva/lib/types";
+import { ReactNode, useCallback, useEffect, useRef, useState } from "react";
+import { Circle, Layer, Rect, Shape, Stage } from "react-konva";
+import { getRandomInt } from "./hooks";
+import { KonvaEventObject } from "konva/lib/Node";
 
-const { DrawCommandTag } = wasmCore;
-
-interface FontObject {
-  fm: wasmCore.FontManager;
-  metrics: wasmCore.FontMetrics;
+interface Transform {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
 }
 
-const FontObjectContext = createContext<FontObject | null>(null);
-
-function useFontObject() {
-  return useContext(FontObjectContext)!;
+interface ComponentBase {
+  (props: { transform: Transform }): ReactNode;
 }
 
-function TextComponent({
-  position,
-  text,
-  fontSize
-}: {
-  position: Vector2d;
-  text: string;
-  fontSize: number;
-}) {
-  const { fm, metrics } = useFontObject();
+interface EditorNode {
+  id: number;
+  transform: Transform;
+  Component: ComponentBase;
+}
 
-  const [outlines, setOutlines] = useState<wasmCore.OutlineRender[]>([]);
-
-  useEffect(() => {
-    if (!isCoreLoaded()) return;
-
-    let outlines = wasmCore.fm_render_string(fm, text);
-    setOutlines(outlines);
-  }, [text]);
-
-  let scale = (1.0 / metrics.upm) * fontSize;
-  let baselineY = metrics.ascender * scale;
-
-  function transform({ x, y }: Vector2d, left: number): Vector2d {
-    return {
-      x: left + x * scale,
-      y: baselineY - y * scale
-    };
-  }
-
+const RectComponent: ComponentBase = ({ transform }) => {
   return (
-    <Shape
-      x={position.x}
-      y={position.y}
+    <Rect
+      x={transform.x}
+      y={transform.y}
+      width={transform.width}
+      height={transform.height}
       fill='black'
-      hitFunc={(context, shape) => {
-        let height = (metrics.ascender - metrics.descender) * scale;
-        let width =
-          outlines.reduce((prev, outline) => prev + outline.advance_width, 0) *
-          scale;
-
-        let pad = 10;
-
-        context.beginPath();
-        context.rect(-pad, -pad, width + pad * 2, height + pad * 2);
-        context.closePath();
-        context.fillStrokeShape(shape);
-      }}
-      sceneFunc={(context, shape) => {
-        if (outlines.length === 0) return;
-
-        context.beginPath();
-
-        let left = 0;
-
-        for (let i = 0; i < outlines.length; ++i) {
-          let outline = outlines[i];
-          outline.commands.forEach(cmd => {
-            let point = transform(cmd.point, left);
-            let control1 = transform(cmd.control1, left);
-            let control2 = transform(cmd.control2, left);
-
-            if (cmd.tag == DrawCommandTag.MoveTo)
-              context.moveTo(point.x, point.y);
-            else if (cmd.tag == DrawCommandTag.LineTo)
-              context.lineTo(point.x, point.y);
-            else if (cmd.tag == DrawCommandTag.QuadTo)
-              context.quadraticCurveTo(
-                control1.x,
-                control1.y,
-                point.x,
-                point.y
-              );
-            else if (cmd.tag == DrawCommandTag.CurveTo)
-              context.bezierCurveTo(
-                control1.x,
-                control1.y,
-                control2.x,
-                control2.y,
-                point.x,
-                point.y
-              );
-            else if (cmd.tag == DrawCommandTag.Close) context.closePath();
-          });
-
-          left += outline.advance_width * scale;
-        }
-
-        context.fillStrokeShape(shape);
-      }}
     />
   );
-}
+};
+
+const CircleComponent: ComponentBase = ({ transform }) => {
+  return (
+    <Circle
+      x={transform.x}
+      y={transform.y}
+      radius={transform.width / 2}
+      fill='green'
+    />
+  );
+};
 
 function Canvas({ width, height }) {
   const stageRef = useRef<Konva.Stage | null>(null);
   const layerRef = useRef<Konva.Layer | null>(null);
 
-  const fontObject: FontObject = useObject(() => {
-    let fm = wasmCore.fm_create();
-    let metrics = wasmCore.fm_metrics(fm);
-    return {
-      fm,
-      metrics
-    };
-  });
+  const [nodes, setNodes] = useState<EditorNode[]>([]);
+  const lastId = useRef(0);
+
+  const addRandomNode = useCallback(
+    (event: KonvaEventObject<MouseEvent>) => {
+      let { x, y } = event.target.getStage()?.getPointerPosition() || {
+        x: 0,
+        y: 0
+      };
+
+      let rectWidth = getRandomInt(10, width * 0.2);
+      let rectHeight = getRandomInt(10, height * 0.2);
+
+      x -= rectWidth / 2;
+      y -= rectHeight / 2;
+
+      let node: EditorNode = {
+        id: lastId.current++,
+        transform: {
+          x,
+          y,
+          width: rectWidth,
+          height: rectHeight
+        },
+        Component: RectComponent
+      };
+
+      setNodes(oldNodes => [...oldNodes, node]);
+    },
+    [width, height]
+  );
 
   return (
-    <FontObjectContext.Provider value={fontObject}>
-      <Stage
-        ref={stageRef}
-        // onClick={() => layerRef.current?.toggleHitCanvas()}
-        width={width}
-        height={height}
-      >
-        <Layer ref={layerRef}>
-          <TextComponent
-            text={"They looked up at the sky and saw a million stars."}
-            fontSize={20}
-            position={{ x: 100, y: 100 }}
-          />
-        </Layer>
-      </Stage>
-    </FontObjectContext.Provider>
+    <Stage ref={stageRef} onClick={addRandomNode} width={width} height={height}>
+      <Layer ref={layerRef}>
+        {nodes.map(node => (
+          <node.Component key={node.id} transform={node.transform} />
+        ))}
+      </Layer>
+    </Stage>
   );
 }
 
--- src/features/hooks.ts
+++ src/features/hooks.ts
@@ -19,3 +19,9 @@ export function useFirstRender() {
 
   return firstRender.current;
 }
+
+export function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
